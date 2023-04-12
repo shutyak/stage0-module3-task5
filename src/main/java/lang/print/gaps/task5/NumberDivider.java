3ba69b30858d66d0a404d3aee73b7fee27f4fc28
vlshutyak <vl.shutyak@gmail.com>
2023-04-12 18:55:21 +0300

    s0-m3-t5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float a=(float) toBeDivided/5;
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        new NumberDivider().divide(9);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        System.out.println(minutes*60);
+    }
+
+    public static void main(String[] args) {
+        new TimeConvertor().convert(11);
     }
 }
